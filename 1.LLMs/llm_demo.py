49db48827bbfe54aff8d073f5a0c0c2c9be53bd0
swatirameshpatil <112795455+swatirameshpatil@users.noreply.github.com>
2026-04-02 17:56:54 +0530

    added promt as parameter

--- 1.LLMs/llm_demo.py
+++ 1.LLMs/llm_demo.py
@@ -4,5 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = OpenAI(model='gpt-3.5-turbo-instruct')
-result = llm.invoke("what is capital of India")
+prompt = "You are log analyzer, you have to analyze the log, find the RCA from logs and give the solution"
+logsample= " 2023-10-01 10:00:00 ERROR: Database connection failed\n2023-10-01 10:05:00 ERROR: Timeout while connecting to the database\n2023-10-01 10:10:00 ERROR: Database connection failed\n"
+prompt = f"{prompt}\n\nHere are the logs:\n{logsample}\n\nPlease analyze the logs and provide the RCA and solution."
+result = llm.invoke(prompt)
 print(result)
